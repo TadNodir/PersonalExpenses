78d004fbf26bbbe74126f7f9478ba570bb903df5
TadNodir <74507609+TadNodir@users.noreply.github.com>
2021-04-22 04:12:15 +0500

    Update ExpenditureTab.sql

--- src/main/resources/ExpenditureTab.sql
+++ src/main/resources/ExpenditureTab.sql
@@ -1,6 +1,2 @@
 SET GLOBAL time_zone = '+5:00';
-INSERT INTO spendingsdb.expenditure (rent, hobby, bills) VALUES (34, 34, 546);
-INSERT INTO spendingsdb.expenditure (food, hobby, bills) VALUES (34, 34, 546);
-INSERT INTO spendingsdb.expenditure (rent, clothes, bills) VALUES (34, 34, 546);
-INSERT INTO spendingsdb.expenditure (rent, hobby, entertainment) VALUES (34, 34, 546);
 SELECT * FROM spendingsdb.expenditure;
